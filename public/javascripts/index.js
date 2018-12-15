b2bbbc5c54fdf289f22287ea4317c8603215fb60
gnehs <you@example.com>
2018-12-15 21:13:34 +0800

    優化前端介面、新增幣幣數數

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -12,8 +12,10 @@ $(() => {
     }
 
     function updateData(count) {
+        let startNumber = parseInt($("main .counter").attr('bibi-count'))
         $("main .counter").attr('bibi-count', count)
-        $("main .counter").text(count.toLocaleString('en-US'))
+        countNum($("main .counter"), startNumber, count)
+        //$("main .counter").text(count.toLocaleString('en-US'))
     }
     setInterval(async () => {
         let counterData = await axios('/count')
@@ -24,10 +26,34 @@ $(() => {
     cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
         loadJS('/javascripts/konamibibi.js')
     });
+    cheet('c o i n 3 x', function () {
+        $("main button.bibi").text('幣幣 ❤️')
+    });
 })
 
 function loadJS(js) {
     s = document.createElement('script');
     s.src = js
     document.getElementsByTagName('body')[0].appendChild(s);
+}
+
+function countNum($el, from, to) {
+    if (to - from < 5) {
+        $el.text(to.toLocaleString('en-US'))
+    } else {
+        $({
+            countNum: from
+        }).animate({
+            countNum: to
+        }, {
+            duration: 500,
+            easing: 'linear',
+            step: function () {
+                $el.text(Math.floor(this.countNum).toLocaleString('en-US'))
+            },
+            complete: function () {
+                $el.text(this.countNum.toLocaleString('en-US'))
+            }
+        });
+    }
 }
